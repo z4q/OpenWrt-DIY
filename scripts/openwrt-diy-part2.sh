6ff9eed65ca5cec6c5eb7c90a3b942554b765210
Ruzaini <ruzaini@jobstore.com>
2021-02-20 15:06:40 +0800

    :zap: add new Mi

--- scripts/openwrt-diy-part2.sh
+++ scripts/openwrt-diy-part2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Clone Lean's latest sources.
 pushd package
